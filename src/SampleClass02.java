add04ab1c911456ba2b1ed403acd78fcdd284b61
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-14 18:04:33 -0500

    Added a couple of lines of code to the SampleClass02 File.

--- src/SampleClass02.java
+++ src/SampleClass02.java
@@ -3,5 +3,8 @@ public class SampleClass02 {
 
 	public static void main(String[] args) {
 		System.out.println("This is a new Java Class File");
+		int i = 10;
+		int j = 25;
+		System.out.println("The Sum of i and j is : "+ i + j);
 	}
 }
