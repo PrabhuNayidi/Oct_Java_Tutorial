51bbe80bb0cfda7a5ea37f341850864edde738ca
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-14 20:28:24 -0500

    New File Division
    New file Division.java was added

---
+++ src/Division.java
@@ -0,0 +1,11 @@
+
+public class Division {
+
+	public static void main(String[] args) {
+		int x = 25;
+		int y = 5;
+		int div = x/y;
+		System.out.println("The Division between x and y is : " + div);
+	}
+
+}
