013f04fa4ba88b744de8f3a7489b91a4f4c0258b
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-14 20:46:41 -0500

    New Java Class File
    A new java class on Primitive Data Types was added

---
+++ src/PrimitiveDataTypes.java
@@ -0,0 +1,51 @@
+
+public class PrimitiveDataTypes {
+
+	private static final boolean True = false;
+	private static final boolean False = false;
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=1;b=2;c=3;
+		int x=10,y,z;
+		y=100;z=1000;
+		int s=25;
+		int f=50;
+		System.out.println("The Value of a is : " + a);
+		System.out.println("The Value of b is : " + b);
+		System.out.println("The Value of c is : " + c);
+		System.out.println("The Value of x is : " + x);
+		System.out.println("The Value of y is : " + y);
+		System.out.println("The Value of z is : " + z);
+		System.out.println("The Value of s is : " + s);
+		System.out.println("The Value of f is : " + f);
+
+		double f1 = 10.00;
+		double d,t,u;
+		d = 25.66668; t  = 89.26587953; u = 25.235689875421;
+		System.out.println("The Value of f1 is : " + f1);
+		System.out.println("The Value of d is : " + d);
+		System.out.println("The Value of t is : " + t);
+		System.out.println("The Value of u is : " + u);
+		
+		char s1 = 'a';
+		char s2 = 'A';
+		char s3 = '0';
+		char s4 = '/';
+		char s5 = '`';
+		char s6 = '&';
+		System.out.println("The value of the given character is : " + s1);
+		System.out.println("The value of the given character is : " + s2);
+		System.out.println("The value of the given character is : " + s3);
+		System.out.println("The value of the given character is : " + s4);
+		System.out.println("The value of the given character is : " + s5);
+		System.out.println("The value of the given character is : " + s6);
+		
+		boolean b2 = False;
+		boolean b1 = True;
+		
+		System.out.println("The value of b1 is : " + b1);
+		System.out.println("The value of b2 is : " + b2);
+	}
+
+}
